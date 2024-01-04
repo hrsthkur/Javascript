39c380f42e5cfbacbd6726a5d2fe3e19b939a758
Harsh Singh <hrsthkur1@gmail.com>
2024-01-04 16:07:55 +0530

    operators

--- test.js
+++ test.js
@@ -10,9 +10,24 @@ console.log(num);*/
 //"33"->33
 //"33abc"->NaN not a number
 //true->1
-let isloggedin=""
-let boolisloggedin = Boolean(isloggedin)
-console.log(typeof boolisloggedin);
+// let isloggedin=""
+// let boolisloggedin = Boolean(isloggedin)
+// console.log(typeof boolisloggedin);
 //""->false
 //"Hitesh"=> true;
 //0->false
+//***********operation***********/
+// let value=33
+// let negvalue=-value
+// console.log(negvalue);
+// console.log(2**3);//power
+
+// let str1="harsh"
+// let str2= "  hehe"
+// console.log(str1+str2);
+// console.log("1"+2+2); 
+// console.log(2+2+"1");
+//jo bhi data type phele hai usme conversion hp jayega like string shuru me to string hi
+console.log(+true);
+console.log(+"");
+//+ is converting it into number
