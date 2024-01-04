653e0c31a7f1fae9999bed8b23d40425e69f5e53
Harsh Singh <hrsthkur1@gmail.com>
2024-01-04 15:40:20 +0530

    learned conversion of datatypes

---
+++ test.js
@@ -0,0 +1,18 @@
+//let score=24;
+//console.log(score);
+//console.log(typeof(score));
+/*console.log("trying to give output");
+let score=undefined
+console.log(typeof(score));
+let num=Number(score);
+console.log(typeof(num))
+console.log(num);*/
+//"33"->33
+//"33abc"->NaN not a number
+//true->1
+let isloggedin=""
+let boolisloggedin = Boolean(isloggedin)
+console.log(typeof boolisloggedin);
+//""->false
+//"Hitesh"=> true;
+//0->false
