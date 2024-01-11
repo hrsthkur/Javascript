5d26cfde013135aefb4c25bbe5ca56deec470747
Harsh Singh <hrsthkur1@gmail.com>
2024-01-11 18:44:53 +0530

    Studied about math(library)

--- 4_numbers.js
+++ 4_numbers.js
@@ -1,13 +1,28 @@
-const score = 400
-//another defining term
-const anotherscore = new Number(100)
-console.log(score) //output = 400
-console.log(anotherscore) //output with given data type i.e Number 100
+// const score = 400
+// //another defining term
+// const anotherscore = new Number(100)
+// console.log(score) //output = 400
+// console.log(anotherscore) //output with given data type i.e Number 100
 
-console.log(score.toString());
-console.log(typeof(score.toString())); //convert it to string but remember stack memory
-console.log(score.toFixed(4)); //for precision value or number of decimals
-const newnumber = 12.34778292
-console.log(newnumber.toPrecision(3)); //gives round off upto 3 or n digits including before decimal
-const hundred = 100000000000;
-console.log(hundred.toLocaleString()); //according to us standards can be changed to indian by adding 'en-IN' in ()
+// console.log(score.toString());
+// console.log(typeof(score.toString())); //convert it to string but remember stack memory
+// console.log(score.toFixed(4)); //for precision value or number of decimals
+// const newnumber = 12.34778292
+// console.log(newnumber.toPrecision(3)); //gives round off upto 3 or n digits including before decimal
+// const hundred = 100000000000;
+// console.log(hundred.toLocaleString()); //according to us standards can be changed to indian by adding 'en-IN' in ()
+
+// console.log(Math.ceil(4.1));
+// //ceil returns round off greater than or top value
+// console.log(Math.floor(4.6));
+// console.log(Math.round(4.6))
+
+// console.log(Math.random()); //gives value from 0 to 1 (included)
+// console.log(Math.floor(Math.random()*10)); 
+
+//********* */
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+//this formula gives values from min to max #EEMPORTENT 😚
+
