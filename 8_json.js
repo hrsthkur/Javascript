4a02612dee8ac75b783199a8023d947ced630e43
Harsh Singh <hrsthkur1@gmail.com>
2024-02-25 12:02:14 +0530

    json talk

---
+++ 8_json.js
@@ -0,0 +1,23 @@
+const course =  {
+    coursename: "Js in Hindi",
+    price: "999",
+    coursestudent: "Harsh"
+}
+
+//console.log(course.coursestudent)
+const {coursestudent} = course
+console.log(coursestudent)
+//direct or destructured
+const {coursestudent: check} = course
+console.log(check)
+
+//json java script object notation
+{
+    //json
+    "name": "harsh"
+    //name is alson in form of string all the values in api are in json
+}
+
+
+
+
