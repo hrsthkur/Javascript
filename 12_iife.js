5da85920028eca1b81aa0178aff5004bcafacdf4
Harsh Singh <hrsthkur1@gmail.com>
2024-02-26 17:23:46 +0530

    iife

--- 12_iife.js
+++ 12_iife.js
@@ -5,8 +5,11 @@
 
 (function check(){
     console.log("going good");
-}) ();   //semicolon is used to end the envoked function
+}) ();   //semicolon is used to end the envoked function use when two iife is used // function is just called after declaration ie immediately
+   
+//named iife upr vala, niche vala un-named iife
 
-(() => {
-    console.log("check this");
-}) ();
+
+((name) => {
+    console.log(`check this ${name}`);
+}) ("Harsh");   //value passing like any other function
