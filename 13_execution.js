e019c301461ee30b3cbcbbce5f28d68c26d46f2f
Harsh Singh <hrsthkur1@gmail.com>
2024-02-27 18:19:22 +0530

    about execution of program

---
+++ 13_execution.js
@@ -0,0 +1,10 @@
+//execution occurs in three phase
+//1 - Global execution --> assign to "this" keyword
+//2 - Memory Phase then memory is assigned to values (only memory not value) they are assigned undefined
+//and the functions in the program are not assigned undefined thier definition is passed
+//3 - execution phase then values are given that are assigned undefined and for the function that are called and 
+//all the operations or calculations are done
+//execution context of function is deleted after return
+//these steps are repeated same  --> new variable enivornment then execution thread => memory phase then execution phase
+
+/********************* call stack is like stack in c++  ************************/
